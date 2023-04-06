545f2d228ef6602cc54d9f2195fc91e55834a69d
yazyounas <yazyounas@gmail.com>
2023-04-06 23:44:44 +0100

    setting up to hoist DataBase

--- listen.js
+++ listen.js
@@ -1,8 +1,4 @@
-const app =require('./app.js')
+const app = require('./app.js');
+const { PORT = 9090 } = process.env;
 
-app.listen(9090,(err) => {
-    if(err) {
-        throw err;
-    }
-    console.log("server is listening")
-})
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
